52109b68e72db6703efb5d5f7465d104ca1315c0
CptPie <CptPie@users.noreply.github.com>
2017-07-05 11:10:22 +0200

    cleaned up code and removed unnecessary global arrays

--- bot.js
+++ bot.js
@@ -2,28 +2,22 @@ const TelegramBot = require('node-telegram-bot-api');
 const request = require('request');
 const schedule = require('node-schedule');
 const fs = require('fs');
-var config = require('./config');
-
-const token = config.bottoken;
-const bot = new TelegramBot(token, {polling: true})
-var dailyloc = config.dailyLocation;
+const config = require('./config');
+const bot = new TelegramBot(config.bottoken, {polling: true})
+const contents = require('./contents')
 
 bot.onText(/\/greet (.+)/, (msg, input) => {
 	const chatId = msg.chat.id;
 	var blub = input[1];
 	var resp ='Hello, '+blub;
-
 	bot.sendMessage(chatId, resp);
 });
 
-var loremSettings = {
+bot.onText(/\/lorem/||/\/lorem (.*)/, (msg) =>{
+	var loremSettings = {
 	url: 'http://lorempixel.com/400/200',
 	encoding: null
-};
-
-
-
-bot.onText(/\/lorem/||/\/lorem (.*)/, (msg) =>{
+	};
 	const chatId = msg.chat.id;
 	request(loremSettings, function (error, response, buffer) {
 		if (!error && response.statusCode == 200){	
@@ -32,18 +26,18 @@ bot.onText(/\/lorem/||/\/lorem (.*)/, (msg) =>{
 	})
 });
 
-var doit = [
+bot.onText(/\/doit/,(msg) =>{
+	var doit = [
 	'http://i.imgur.com/RPbK0fZ.png',
 	'http://i.imgur.com/TgUQfNI.jpg',
 	'http://i.imgur.com/WSm116p.jpg',
-]
-
-bot.onText(/\/doit/,(msg) =>{
+	];
 	const chatId = msg.chat.id;
 	bot.sendPhoto(chatId, doit[Math.floor(Math.random()*doit.length)]);
 });
 
-var ship = [
+bot.onText(/\/ship/,(msg) =>{
+	var ship = [
 	'http://i.imgur.com/b7bzy0C.png',
 	'http://i.imgur.com/hse69ui.jpg',
 	'http://i.imgur.com/pyjpvhM.gif',
@@ -74,14 +68,13 @@ var ship = [
 	'https://media4.giphy.com/media/ta83CqOoRwfwQ/giphy.gif',
 	'https://media2.giphy.com/media/tDafHUBVrRKtq/giphy.gif',
 	'https://media2.giphy.com/media/tDafHUBVrRKtq/giphy.gif',
-]
-
-bot.onText(/\/ship/,(msg) =>{
+	];
 	const chatId = msg.chat.id;
 	bot.sendMessage(chatId, ship[Math.floor(Math.random()*ship.length)]);
 });
 
-var editions = [
+bot.onText(/\/draw/, (msg) =>{
+	var editions = [
 	{short:'an', count:92},
 	{short:'lg', count:310},
 	{short:'dk', count:119},
@@ -115,38 +108,32 @@ var editions = [
 	{short:'p3k', count:180},
 	{short:'ced', count:302},
 	{short:'cedi', count:302},
-]
-
-bot.onText(/\/draw/, (msg) =>{
+	];
 	const chatId = msg.chat.id;
 	let edition = editions[Math.floor(Math.random()*editions.length)];
 	bot.sendMessage(chatId,`http://magiccards.info/scans/en/${edition.short}/${Math.floor(Math.random()*edition.count)}.jpg`);
 });
-
-
-
-
-var merge = [
-	'https://i.imgur.com/X9zNSkM.gif',
-	'https://cdn.meme.am/cache/instances/folder86/500x/64007086/disaster-girl-push-rejected-rebase-or-merge-git-push-force.jpg',
-	'https://www.mememaker.net/static/images/memes/3629643.jpg',
-	'http://s.quickmeme.com/img/58/58cdc50dcb04a16438e9759f2db7a0ac23442e718f69af7b2ac0a3761915e92a.jpg',
-	'https://i.stack.imgur.com/nEfIm.jpg',
-]
-
+//does this function have to exist ... (CptPie)
 bot.onText(/\/💩/,(msg) => {
 	const chatId = msg.chat.id;
 	bot.sendMessage(chatId, 'Kothaufen');
 });
 
 bot.onText(/\/merge/,(msg) => {
+	var merge = [
+	'https://i.imgur.com/X9zNSkM.gif',
+	'https://cdn.meme.am/cache/instances/folder86/500x/64007086/disaster-girl-push-rejected-rebase-or-merge-git-push-force.jpg',
+	'https://www.mememaker.net/static/images/memes/3629643.jpg',
+	'http://s.quickmeme.com/img/58/58cdc50dcb04a16438e9759f2db7a0ac23442e718f69af7b2ac0a3761915e92a.jpg',
+	'https://i.stack.imgur.com/nEfIm.jpg',
+	];
 	const chatId = msg.chat.id;
 	bot.sendMessage(chatId, merge[Math.floor(Math.random()*merge.length)]);
 });
 
 var j = schedule.scheduleJob('30 5 * * *', function(){
-	downloader('http://wttr.in/'+dailyloc+'.png?1', 'dailywetter.png',function(){
-		sendingWeather(config.dailyChatId,dailyloc,__dirname+'/dailywetter.png');
+	downloader('http://wttr.in/'+config.dailyLocation+'.png?1', 'dailywetter.png',function(){
+		sendingWeather(config.dailyChatId,dailyLocation,__dirname+'/dailywetter.png');
 	});
 });
 
@@ -164,23 +151,26 @@ bot.onText(/\/weather (.+)/, (msg, input) => {
 	});
 });
 
+function sendingWeather(chatId,loc, photo){
+	bot.sendPhoto(chatId, photo, {caption: "Todays weather forecast for: "+loc});
+};
+
 bot.onText(/\/decide (.+)/, (msg, input) => {
 	const chatID = msg.chat.id;
 	var answer = Math.random() >= 0.5 ? "Yes" : "No";
 	bot.sendMessage(chatID, input[1] + " " + answer);
 });
 
-bot.onText(/\/marius/, (msg) => {
+bot.onText(/\/burn/, (msg) => {
 	const chatID = msg.chat.id;
 	bot.sendMessage(chatID, "https://i.giphy.com/media/l2YWsiql5xGPIbnzy/giphy.gif");
 });
+
 bot.onText(/\/slap (.+)/, (msg,input) => {
 	const chatID = msg.chat.id;
 	bot.sendMessage(chatID, "<b>" + msg.from.first_name + " slaps " + input[1] + " around a bit with a large trout</b>",{parse_mode : "HTML"});
 });
-function sendingWeather(chatId,loc, photo){
-	bot.sendPhoto(chatId, photo, {caption: "Todays weather forecast for: "+loc});
-};
+
 
 bot.onText(/\/hype/, (msg) =>{
 	bot.sendMessage(msg.chat.id, msg.from.first_name+' started a HYPE-Train!');
@@ -192,42 +182,36 @@ bot.onText(/\/choose (.+), (.+)/,(msg, input) => {
 	bot.sendMessage(msg.chat.id, "@shipitbot has chosen *"+answer+"* for "+msg.from.first_name,{parse_mode : "Markdown"});
 });
 
-var geste = [
-	'Schere',
-	'Stein',
-	'Papier',
-]
-
-bot.onText(/\/schere/, (msg) =>{
-	let gestenzeichen = geste[Math.floor(Math.random()*geste.length)];
-	if (gestenzeichen == 'Schere') {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht eine Schere, '+msg.from.first_name+' hat ein Unentschieden erzielt');
-	} else if (gestenzeichen == 'Stein') {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht einen Stein, '+msg.from.first_name+' hat verloren');
+bot.onText(/\/scissors/, (msg) =>{
+	let sign = contents.sign[Math.floor(Math.random()*Object.keys(contents.sign).length)].name;
+	if (sign == 'scissors') {
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose scissors, it\'s a draw between ShipIt-Bot and '+msg.from.first_name);
+	} else if (sign == 'rock') {
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose rock, '+msg.from.first_name+' has lost');
 	} else {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht Papier, '+msg.from.first_name+' hat GEWONNEN');
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose paper, '+msg.from.first_name+' has won');
 	}
 });
 
-bot.onText(/\/stein/, (msg) =>{
-	let gestenzeichen = geste[Math.floor(Math.random()*geste.length)];
-	if (gestenzeichen == 'Schere') {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht eine Schere, '+msg.from.first_name+' hat GEWONNEN');
-	} else if (gestenzeichen == 'Stein') {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht einen Stein, '+msg.from.first_name+' hat ein Unentschieden erzielt');
+bot.onText(/\/rock/, (msg) =>{
+	let sign = contents.sign[Math.floor(Math.random()*Object.keys(contents.sign).length)].name;
+	if (sign == 'scissors') {
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose scissors, '+msg.from.first_name+' has won');
+	} else if (sign == 'rock') {
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose rock, it\'s a draw between ShipIt-Bot and '+msg.from.first_name);
 	} else {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht Papier, '+msg.from.first_name+' hat verloren');
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose paper, '+msg.from.first_name+' has lost');
 	}
 });
 
-bot.onText(/\/papier/, (msg) =>{
-	let gestenzeichen = geste[Math.floor(Math.random()*geste.length)];
-	if (gestenzeichen == 'Schere') {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht eine Schere, '+msg.from.first_name+' hat verloren');
-	} else if (gestenzeichen == 'Stein') {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht einen Stein, '+msg.from.first_name+' hat GEWONNEN');
+bot.onText(/\/paper/, (msg) =>{
+	let sign = contents.sign[Math.floor(Math.random()*Object.keys(contents.sign).length)].name;
+	if (sign == 'scissors') {
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose scissors, '+msg.from.first_name+' has lost');
+	} else if (sign == 'rock') {
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose rock, '+msg.from.first_name+' has won');
 	} else {
-		bot.sendMessage(msg.chat.id, 'ShipIt-Bot macht Papier, '+msg.from.first_name+' hat ein Unentschieden erzielt');
+		bot.sendMessage(msg.chat.id, 'ShipIt-Bot chose paper, it\'s a draw between ShipIt-Bot and '+msg.from.first_name);
 	}
 });
 
