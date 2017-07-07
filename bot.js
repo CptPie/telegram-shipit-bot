6a05a838a593fb142029d841357595c8d885e1e2
CptPie <CptPie@users.noreply.github.com>
2017-07-07 08:55:35 +0200

    moved all arrays to contents.json && fixed #28

--- bot.js
+++ bot.js
@@ -45,92 +45,21 @@ bot.onText(/\/lorem/||/\/lorem (.*)/, (msg) =>{
 
 bot.onText(/\/doit/,(msg) =>{
 	saveUser(msg.from.username, msg.from.first_name);
-	var doit = [
-	'http://i.imgur.com/RPbK0fZ.png',
-	'http://i.imgur.com/TgUQfNI.jpg',
-	'http://i.imgur.com/WSm116p.jpg',
-	];
 	const chatId = msg.chat.id;
-	bot.sendPhoto(chatId, doit[Math.floor(Math.random()*doit.length)]);
+	bot.sendPhoto(chatId, contents.doit[Math.floor(Math.random()*contents.doit.length)].link);
 });
 
 bot.onText(/\/ship/,(msg) =>{
 	saveUser(msg.from.username, msg.from.first_name);
-	var ship = [
-	'http://i.imgur.com/b7bzy0C.png',
-	'http://i.imgur.com/hse69ui.jpg',
-	'http://i.imgur.com/pyjpvhM.gif',
-	'http://i.imgur.com/66j50Xz.jpg',
-	'http://i.imgur.com/tCiQSlg.jpg',
-	'http://i.imgur.com/DDLLoL8.gif',
-	'http://i.imgur.com/LDzcDY3.jpg',
-	'http://i.imgur.com/nkeMpLe.jpg',
-	'http://i.imgur.com/ACaARFw.jpg',
-	'http://i.imgur.com/aJkhUrE.jpg',
-	'http://i.imgur.com/YBdJsaS.jpg',
-	'http://i.imgur.com/KmZrtgd.jpg',
-	'http://i.imgur.com/rmgCY2Q.jpg',
-	'http://i.imgur.com/g00lVs9.gif',
-	'http://i.imgur.com/LIIXWmD.png',
-	'http://i.imgur.com/c6K9rP3.jpg',
-	'http://i.imgur.com/2M8iyPt.jpg',
-	'http://i.imgur.com/7XxnrTu.jpg',
-	'http://i.imgur.com/AJN04yl.jpg',
-	'http://i.imgur.com/rcIMpM0.gif',
-	'http://i.imgur.com/60u8kye.jpg',
-	'http://i.imgur.com/yUypmaC.jpg',
-	'http://i.imgur.com/IflZleY.jpg',
-	'http://i.imgur.com/h3mlvCq.png',
-	'http://i.imgur.com/dXzZETc.png',
-	'https://media.giphy.com/media/143vPc6b08locw/giphy.gif',
-	'https://media0.giphy.com/media/14tFXhhNurdGp2/giphy.gif',
-	'https://media4.giphy.com/media/ta83CqOoRwfwQ/giphy.gif',
-	'https://media2.giphy.com/media/tDafHUBVrRKtq/giphy.gif',
-	'https://media2.giphy.com/media/tDafHUBVrRKtq/giphy.gif',
-	];
 	const chatId = msg.chat.id;
-	bot.sendMessage(chatId, ship[Math.floor(Math.random()*ship.length)]);
+	bot.sendMessage(chatId, contents.ship[Math.floor(Math.random()*contents.ship.length)].link);
 });
 
 bot.onText(/\/draw/, (msg) =>{
 	saveUser(msg.from.username, msg.from.first_name);
-	var editions = [
-	{short:'an', count:92},
-	{short:'lg', count:310},
-	{short:'dk', count:119},
-	{short:'fe', count:187},
-	{short:'hl', count:140},
-	{short:'mr', count:350},
-	{short:'vi', count:167},
-	{short:'wl', count:167},
-	{short:'tp', count:350},
-	{short:'sh', count:167},
-	{short:'ex', count:167},
-	{short:'us', count:350},
-	{short:'ul', count:143},
-	{short:'ud', count:143},
-	{short:'mm', count:350},
-	{short:'ne', count:143},
-	{short:'pr', count:143},
-	{short:'in', count:355},
-	{short:'ps', count:146},
-	{short:'ap', count:148},
-	{short:'od', count:350},
-	{short:'tr', count:143},
-	{short:'ju', count:143},
-	{short:'on', count:350},
-	{short:'le', count:145},
-	{short:'sc', count:143},
-	{short:'be', count:302},
-	{short:'ug', count:88},
-	{short:'po2', count:165},
-	{short:'po', count:222},
-	{short:'p3k', count:180},
-	{short:'ced', count:302},
-	{short:'cedi', count:302},
-	];
+	
 	const chatId = msg.chat.id;
-	let edition = editions[Math.floor(Math.random()*editions.length)];
+	let edition = contents.editions[Math.floor(Math.random()*contents.editions.length)];
 	bot.sendMessage(chatId,`http://magiccards.info/scans/en/${edition.short}/${Math.floor(Math.random()*edition.count)}.jpg`);
 });
 //does this function have to exist ... (CptPie)
@@ -142,15 +71,8 @@ bot.onText(/\/💩/,(msg) => {
 
 bot.onText(/\/merge/,(msg) => {
 	saveUser(msg.from.username, msg.from.first_name);
-	var merge = [
-	'https://i.imgur.com/X9zNSkM.gif',
-	'https://cdn.meme.am/cache/instances/folder86/500x/64007086/disaster-girl-push-rejected-rebase-or-merge-git-push-force.jpg',
-	'https://www.mememaker.net/static/images/memes/3629643.jpg',
-	'http://s.quickmeme.com/img/58/58cdc50dcb04a16438e9759f2db7a0ac23442e718f69af7b2ac0a3761915e92a.jpg',
-	'https://i.stack.imgur.com/nEfIm.jpg',
-	];
 	const chatId = msg.chat.id;
-	bot.sendMessage(chatId, merge[Math.floor(Math.random()*merge.length)]);
+	bot.sendMessage(chatId, contents.merge[Math.floor(Math.random()*contents.merge.length)].link);
 });
 
 var j = schedule.scheduleJob('30 5 * * *', function(){
