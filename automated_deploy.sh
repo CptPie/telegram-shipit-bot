0c7d1a4f0bb1a0ebeb782d76646ac55b144e0157
Sceinox <sc31n0x@googlemail.com>
2017-09-28 11:26:28 +0200

    removed spacing completly

--- automated_deploy.sh
+++ automated_deploy.sh
@@ -2,18 +2,12 @@
 
 echo "`date` [AD] Started Automated Deploy Script" | tee -a "log.txt"
 
-echo "" | tee -a log.txt
-echo "" | tee -a log.txt
-
 echo "`date` [NPM] updating npm" | tee -a log.txt
 npm update
 echo "`date` [NPM] update done" | tee -a log.txt
 
 echo "`date` [NPM] npm start" | tee -a log.txt
 npm start &
-
-echo "" | tee -a log.txt
-echo "" | tee -a log.txt
 
 while [ 1 ]; do
         git fetch origin
@@ -24,15 +18,9 @@ while [ 1 ]; do
                 echo "`date` [GIT] new commit detected" | tee -a log.txt
                 echo "`date` [PREP] preparing for merge" | tee -a log.txt
                 
-                echo "" | tee -a log.txt
-                echo "" | tee -a log.txt                
-
                 echo "`date` [PREP][NPM] Updating npm packages" | tee -a log.txt
                 npm update
                 echo "`date` [PREP][NPM] npm packages updated" | tee -a log.txt
-
-                echo "" | tee -a log.txt
-                echo "" | tee -a log.txt
 
                 echo "`date` [PREP] New version available" | tee -a log.txt
                 echo "`date` [PREP][PROC] Killing current 'node bot.js' and './make.sh' process" | tee -a log.txt
@@ -40,24 +28,14 @@ while [ 1 ]; do
                 pkill -f "node server.js"
                 echo "`date` [PREP][PROC] Process killed" | tee -a log.txt
 
-                echo "" | tee -a log.txt
-                echo "" | tee -a log.txt
-
                 echo "`date` [GIT][MERGE] Merging changed" | tee -a log.txt
                 git merge origin/master | tee -a log.txt
                 echo "`date` [GIT][MERGE] merging done" | tee -a log.txt
 
                 sleep 10s
 
-                echo "" | tee -a log.txt
-                echo "" | tee -a log.txt
-
                 echo "`date` [BOT] Starting Bot again" | tee -a log.txt
-                echo "" | tee -a log.txt
                 npm start &
-
-                echo "" | tee -a log.txt
-                echo "" | tee -a log.txt
                 
         fi
 
